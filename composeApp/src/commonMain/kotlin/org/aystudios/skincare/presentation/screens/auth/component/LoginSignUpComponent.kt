123a9603b601c49a3e182bcb5303e38e46fa413d
atharvyadav22 <dev.atharvyadav@gmail.com>
2026-01-13 19:05:56 +0530

    Added Custom Toast in Login

--- composeApp/src/commonMain/kotlin/org/aystudios/skincare/presentation/screens/auth/component/LoginSignUpComponent.kt
+++ composeApp/src/commonMain/kotlin/org/aystudios/skincare/presentation/screens/auth/component/LoginSignUpComponent.kt
@@ -1,12 +1,21 @@
 package org.aystudios.skincare.presentation.screens.auth.component
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -33,9 +42,11 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import org.aystudios.skincare.core.network.ApiResult
 import org.aystudios.skincare.presentation.components.getAppNavigator
 import org.aystudios.skincare.presentation.navigation.AppBottomNavigator
@@ -53,82 +64,123 @@ fun LoginSignUpScreenComponent(
     onClick: () -> Unit
 ) {
 
-    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val state by loginViewModel.loginState.collectAsState()
+    val navigator = getAppNavigator()
+    val isLoading = state is ApiResult.Loading
 
-        var email by rememberSaveable { mutableStateOf("") }
-        var password by rememberSaveable { mutableStateOf("") }
-        val state by loginViewModel.loginState.collectAsState()
-        val navigator = getAppNavigator()
-
-        LaunchedEffect(state) {
-            if (state is ApiResult.Success) {
-                navigator.replaceAll(AppBottomNavigator)
-            }
+    var showError by remember { mutableStateOf(false) }
+    LaunchedEffect(state) {
+        if (state is ApiResult.Error) {
+            showError = true
+            delay(3000)
+            showError = false
         }
+    }
 
-        Text(
-            if (isLoginScreen) "Welcome Back Login" else "Sign in to your Account",
-            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.SemiBold)
-        )
-        Text(
-            "Enter you email and password to login here",
-            style = MaterialTheme.typography.labelLarge.copy(color = Color.Gray)
-        )
-
-        Spacer(Modifier.height(4.dp))
-        InputTextComponent(
-            "Email",
-            "example@gmail.com",
-            text = email,
-            keyboardType = KeyboardType.Email
-        ) { email = it }
-        InputTextComponent(
-            "Password",
-            "Enter your password",
-            text = password,
-            keyboardType = KeyboardType.Password,
-            isPassword = true
-        ) { password = it }
-
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { loginViewModel.login(email, password) }
-        )
-        {
-
-            Text(if (isLoginScreen) "Login" else "Sign Up")
+    LaunchedEffect(state) {
+        if (state is ApiResult.Success) {
+            navigator.replaceAll(AppBottomNavigator)
         }
+    }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            HorizontalDivider(modifier = Modifier.weight(1f))
-            Text("Or login with", style = MaterialTheme.typography.labelLarge.copy(Color.Gray))
-            HorizontalDivider(modifier = Modifier.weight(1f))
-        }
+    Box {
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
 
+            Text(
+                if (isLoginScreen) "Welcome Back Login" else "Sign in to your Account",
+                style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.SemiBold)
+            )
+            Text(
+                "Enter you email and password to login here",
+                style = MaterialTheme.typography.labelLarge.copy(color = Color.Gray)
+            )
 
-        val annotatedText = buildAnnotatedString {
-            withStyle(MaterialTheme.typography.labelLarge.copy(Color.Gray).toSpanStyle()) {
-                val helper = if (isLoginScreen) "Don't" else "Already"
-                append(" $helper Have An Account? ")
+            Spacer(Modifier.height(4.dp))
+            InputTextComponent(
+                "Email",
+                "example@gmail.com",
+                text = email,
+                keyboardType = KeyboardType.Email
+            ) { email = it }
+            InputTextComponent(
+                "Password",
+                "Enter your password",
+                text = password,
+                keyboardType = KeyboardType.Password,
+                isPassword = true
+            ) { password = it }
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                enabled = !isLoading,
+                onClick = { loginViewModel.login(email, password) }
+            )
+            {
+
+                Text(if (isLoginScreen) "Login" else if (isLoading) "Logging..." else "Sign Up")
             }
 
-            withStyle(
-                MaterialTheme.typography.labelLarge.copy(
-                    Color(0xFF4D81E7),
-                    textDecoration = TextDecoration.Underline
-                ).toSpanStyle()
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
             ) {
-                append(if (isLoginScreen) "Sign Up" else "Login")
+                HorizontalDivider(modifier = Modifier.weight(1f))
+                Text("Or login with", style = MaterialTheme.typography.labelLarge.copy(Color.Gray))
+                HorizontalDivider(modifier = Modifier.weight(1f))
             }
+
+
+            val annotatedText = buildAnnotatedString {
+                withStyle(MaterialTheme.typography.labelLarge.copy(Color.Gray).toSpanStyle()) {
+                    val helper = if (isLoginScreen) "Don't" else "Already"
+                    append(" $helper Have An Account? ")
+                }
+
+                withStyle(
+                    MaterialTheme.typography.labelLarge.copy(
+                        Color(0xFF4D81E7),
+                        textDecoration = TextDecoration.Underline
+                    ).toSpanStyle()
+                ) {
+                    append(if (isLoginScreen) "Sign Up" else "Login")
+                }
+            }
+            Text(
+                annotatedText,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .clickable { onClick() })
+
         }
-        Text(
-            annotatedText,
+
+        AnimatedVisibility(
+            visible = showError,
+            enter = fadeIn(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = FastOutSlowInEasing
+                )
+            ) + slideInVertically(
+                animationSpec = tween(300),
+                initialOffsetY = { -it }
+            ),
+            exit = fadeOut(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = FastOutSlowInEasing
+                )
+            ) + slideOutVertically(
+                animationSpec = tween(300),
+                targetOffsetY = { -it }
+            ),
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .clickable { onClick() })
+                .align(Alignment.TopCenter)
+        ) {
+            ErrorBannerComponent( (state as ApiResult.Error).message )
+        }
+
     }
 
 }
