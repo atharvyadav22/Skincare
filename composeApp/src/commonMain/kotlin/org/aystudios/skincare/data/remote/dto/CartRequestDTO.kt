b7e3c06baf0396949b601a5535d140afdc7632b2
ATHARV YADAV <dev.atharvyadav@gmail.com>
2026-06-18 14:54:05 +0530

    Complete Cart Migration Logic

--- composeApp/src/commonMain/kotlin/org/aystudios/skincare/data/remote/dto/CartRequestDTO.kt
+++
@@ -1,9 +0,0 @@
-package org.aystudios.skincare.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CartRequestDTO(
-    val productId: Long,
-    val quantity: Int
-)
--- composeApp/src/commonMain/kotlin/org/aystudios/skincare/data/remote/dto/CartResponseDTO.kt
+++
@@ -1,9 +0,0 @@
-package org.aystudios.skincare.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CartResponseDTO(
-    val cartItems: List<CartItemResponseDTO>,
-    val totalPrice: Double
-)
