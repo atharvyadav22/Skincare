46a484d70f5a2dccdc1580fe86a8620d5f2dbcdd
Atharv Yadav <dev.atharvyadav@gmail.com>
2026-09-01 17:37:25 +0530

    Order migration logic

--- composeApp/src/commonMain/kotlin/org/aystudios/skincare/data/remote/dto/OrderRequestDTO.kt
+++
@@ -1,6 +0,0 @@
-package org.aystudios.skincare.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class OrderRequestDTO(val paymentMode: PaymentMode)
--- composeApp/src/commonMain/kotlin/org/aystudios/skincare/data/remote/dto/OrderResponseDTO.kt
+++
@@ -1,6 +0,0 @@
-package org.aystudios.skincare.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class OrderResponseDTO(val orderId: Long, val status: OrderStatus, val paymentMode: PaymentMode, val price: Double)
