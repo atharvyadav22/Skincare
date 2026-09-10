1d442a8ca9ca610af14acfcc26356c6411801004
Atharv Yadav <dev.atharvyadav@gmail.com>
2026-09-10 12:42:29 +0530

    added few components

--- composeApp/src/commonMain/kotlin/org/aystudios/skincare/presentation/viewmodels/ListPagingUIState.kt
+++
@@ -1,8 +0,0 @@
-package org.aystudios.skincare.presentation.viewmodels
-
-data class ListPagingUIState<T>(
-    val items: List<T> = emptyList(),
-    val isLoading: Boolean = false,
-    val endReached: Boolean = false,
-    val error: String? = null
-)
